1ccc8ae9f4acbeb52ef9c1ed0728d95d27d7bb59
Jonathan <jramirez@icstars.org>
2025-04-03 15:58:08 -0500

    added a readme template

--- README.md
+++ README.md
@@ -3,3 +3,164 @@
 Jonathan Ramirez
 Cycle 53
 I will add more later
+
+
+# CYD Website
+
+**Table of Contents**
+
+- [Getting Started](#️-getting-started)
+- [Contributing](#️-contributing)
+- [Development Guidelines](#️-development-guidelines)
+- [Quick Tips](#️-quick-tips)
+- [Additional Info for HYD Participants](#️-hack-your-dreams-hyd-2025-participants)
+
+## ⭐️ Getting Started
+
+### Prerequisites
+
+1. Install **Node.js** and **npm**: [Download here](https://nodejs.org/en/download).
+2. Set up SSH with GitHub: [Generate](https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent) and [add](https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account) an SSH key.
+3. Install **VSCode**: [Download here](https://code.visualstudio.com/).
+
+### Local setup
+
+1. Clone the repository:
+   ```bash
+   git clone <SSH_URL>
+   cd cyd-website
+   ```
+2. Download the `.env` file from [Google Drive](https://drive.google.com/drive/folders/1z2poGZZ4FMGLaFsqRV_0cHIHu6Z9lytm?usp=sharing) and place it in the `cyd-website` folder.
+   - If access is restricted, provide your email for access.
+   - **Important**: Do not share these credentials or commit them to GitHub.
+3. Open the project in VSCode:
+   - Open the `cyd-website` folder in Visual Studio Code.
+   - Ensure the terminal in VSCode is set to the root `cyd-website` folder.
+4. Install dependencies and start the development server:
+   ```bash
+   npm install
+   npm run dev
+   ```
+5. Visit `http://localhost:3000` in your browser to view the app.
+
+---
+
+## ⭐️ Contributing
+
+### Workflow
+
+1. **Switch to the appropriate main branch**:
+   ```bash
+   git checkout main-<project_name>
+   git pull origin main-<project_name>
+   ```
+2. **Create a feature branch**:
+   ```bash
+   git checkout -b main-<project_name>/<your-branch-name>
+   ```
+   > _Replace `<project_name>` with your project name and `<your-branch-name>` with a descriptive name for your work, e.g., `main-hyd-career/update-home-body`._
+3. **Commit changes**:
+   ```bash
+   git add .
+   git commit -m "Description of changes"
+   ```
+4. **Push your branch**:
+   ```bash
+   git push origin main-<project_name>/<your-branch-name>
+   ```
+   > ‼️ _Always specify the remote and branch to avoid pushing to the wrong branch!_
+5. **Open a Pull Request (PR)**:
+   - Go to the [Pull Request page](https://github.com/CodeYourDreams/cyd-website/compare) and create a new PR to merge changes from your feature branch (`main-<project_name>/<feature_name>`) into the main branch for your project (`main-<project_name>`).
+   - Provide a detailed description and request a review from a team member.
+   - Once they approve your PR, you can merge it into the main branch for your project (`main-<project_name>`).
+
+### Best Practices
+
+- **Code Quality:**
+
+  - Run `npm run prettify` and `npm run lint` before committing changes to format and lint your code.
+  - Write meaningful commit messages and commit often.
+
+- **Pull Requests:**
+  - Add a detailed description to your PR.
+  - Use the "Files Changed" tab on GitHub to add comments explaining your updates.
+
+---
+
+## ⭐️ Development Guidelines
+
+> Learn more about file and folder conventions here: [https://nextjs.org/docs/app/getting-started/project-structure](https://nextjs.org/docs/app/getting-started/project-structure).
+
+### Frontend
+
+1. **File and Folder Conventions**
+
+   - **Pages:** Create front-end pages in `app/<path|slug>/page.tsx`. The file name must be `page.tsx`, and the file path corresponds to the URL slug (the path after the domain name in the browser's URL).
+     - Do not modify `app/page.tsx` or `app/layout.tsx` as these are core files.
+     - Use the `rafce` VSCode extension to quickly generate consistent React components. This ensures everyone follows the same syntax.
+   - **Props:** If a page takes `props` as input, define an `interface` type for props above the function within the same file.
+
+   - **Navigation:** Use `<Link>` instead of the `<a>` tag for client-side navigation. Example:
+     ```jsx
+     <Link href="/users">Users</Link>
+     ```
+
+2. **Components**
+
+   - **Shared Components:** General React components used across the entire site should be placed under the `components/` folder. Do not modify these components unless necessary.
+   - **Page-Specific Components:** Components exclusive to a specific page or project (e.g., classroom portal, peer learning hub) should be stored within the corresponding project folder, e.g., `classroom/components/*.tsx`.
+   - **Naming Convention:** The component file name must match the function name inside the file. Use PascalCase (e.g., `CreateAssignmentForm.tsx`).
+   - **Client-Side Rendering:** Use `'use client'` only when absolutely necessary. If only part of a component requires client-side rendering, isolate that part into its own component.
+
+3. **Styling**
+   - Use [DaisyUI components](https://daisyui.com/components/) to ensure consistent styling. For example, apply `btn` class for buttons or `alert` class for alert modals.
+   - For additional customizations, apply Tailwind CSS class names as needed.
+
+### Backend
+
+1. **Routes**
+
+   - **Route Files:** Create API routes in `app/api/<route_name>/route.tsx`. The `route_name` corresponds to the entity or object you're performing CRUD operations on (e.g., `users`).
+   - **HTTP Requests:** The `route.tsx` files should contain the HTTP request logic (e.g., `GET`, `POST`, `PUT`, `DELETE`). They should not include data storage or retrieval logic.
+   - **Server-Side Data Fetching:** Use `fetch()` for server-side data fetching within these route files.
+
+2. **Database Functions**
+
+   - **Query Files:** Write all database queries related to an entity or object in `lib/query/<object_name>.ts`. For example, `lib/query/users.ts` will contain functions for CRUD operations on the `users` table.
+   - **Calling Queries:** These query functions should be called within the corresponding API routes, e.g., `app/api/users/route.tsx`.
+
+3. **Types**
+   - **Type Definitions:** Define TypeScript types that correspond to the entities in your database tables in the `types/` folder. The file name should match the route name or object name, e.g., `types/users.ts` for user-related types.
+
+---
+
+## ⭐️ Quick Tips
+
+- Use `npm run` to view available commands.
+- Run `npm run prettify` and `npm run lint` before committing to format and lint your code.
+- Always pull the latest main branch before starting a new feature branch.
+- Ensure you're on a feature branch before making any changes.
+- Never commit directly to the main branch.
+- Keep PRs small and focused on one feature. Multiple smaller PRs are better than a single overly complicated, unnecessarily large one.
+- Commit frequently to avoid bundling unrelated changes or cluttering commit messages.
+- Look up solutions and ask for help when needed.
+
+---
+
+## ⭐️ iCAA Collaborators 2025 Participants
+
+Use Notion for collaboration and documentation:
+
+- [HYD 2025 Workspace](https://www.notion.so/invite/3922e9f78eb8a1256d8ec7597d2d5df1a0bd5f82): Includes participants, team wikis, Branding Guidelines, etc.
+- [Onboarding Checklist](): Start here for setup and orientation.
+- _Note: You need to click the "Edit" button at the top-right corner of the Notion page to edit docs. See [How to Edit Notion](./readme-notion-edit.png)._
+
+### Team Branches
+
+Each team has a dedicated branch for collaboration, following this naming convention: `main-iCAA-*`, where `*` represents your team name or your individual name.
+
+**Current Branches**
+
+- [`main-hyd-collaboration`](https://github.com/CodeYourDreams/cyd-website/tree/main-hyd-mentor) – For alumni or residents.
+
+To explore all branches or check their current status, visit the [repository branches page](https://github.com/JValentineC/iCAAhub/branches).
--- app/page.tsx
+++ app/page.tsx
@@ -7,154 +7,132 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="w-full mx-auto min-h-screen bg-base-200">
-
       {/* container */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-full py-4">
-
-
-         
-          {/* This is the first card and doesnt appear for those on their cellphones, it is a responsive image with a background color and a title */}
-          <div className="sr-only lg:not-sr-only justify-center lg:col-span-3">
-            <figure
-              className="max-h-[500px] diff py-3 aspect-16/9"
-              tabIndex={0}
-            >
-              <div className="diff-item-1" role="img">
-                <div className="bg-info image-full text-info-content grid place-content-center pb-5 font-black">
-                  <figure>
-                    <Image
-                      src="/WhiteiCAA.svg"
-                      alt="iCAA"
-                      width={500} // Set an appropriate width
-                      height={300} // Set an appropriate height
-                      layout="responsive" // Dynamically adjusts width and height
-                      objectFit="contain" // Ensures the image scales proportionally
-                    />
-                  </figure>
-                </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-full py-4">
+        {/* This is the first card and doesnt appear for those on their cellphones, it is a responsive image with a background color and a title */}
+        <div className="sr-only lg:not-sr-only justify-center lg:col-span-3">
+          <figure className="max-h-[500px] diff py-3 aspect-16/9" tabIndex={0}>
+            <div className="diff-item-1" role="img">
+              <div className="bg-info image-full text-info-content grid place-content-center pb-5 font-black">
+                <figure>
+                  <Image
+                    src="/WhiteiCAA.svg"
+                    alt="iCAA"
+                    width={500} // Set an appropriate width
+                    height={300} // Set an appropriate height
+                    layout="responsive" // Dynamically adjusts width and height
+                    objectFit="contain" // Ensures the image scales proportionally
+                  />
+                </figure>
               </div>
-              <div className="diff-item-2" role="img" tabIndex={0}>
-                <div className="bg-secondary grid place-content-center image-full pb-5 font-black text-secondary-content">
-                  <figure>
-                    <Image
-                      src="/BlackiCAA.svg"
-                      alt="iCAA"
-                      width={500} // Set an appropriate width
-                      height={300} // Set an appropriate height
-                      layout="responsive" // Dynamically adjusts width and height
-                      objectFit="contain" // Ensures the image scales proportionally
-                    />
-                  </figure>
-                </div>
+            </div>
+            <div className="diff-item-2" role="img" tabIndex={0}>
+              <div className="bg-secondary grid place-content-center image-full pb-5 font-black text-secondary-content">
+                <figure>
+                  <Image
+                    src="/BlackiCAA.svg"
+                    alt="iCAA"
+                    width={500} // Set an appropriate width
+                    height={300} // Set an appropriate height
+                    layout="responsive" // Dynamically adjusts width and height
+                    objectFit="contain" // Ensures the image scales proportionally
+                  />
+                </figure>
               </div>
-              <div className="diff-resizer"></div>
-            </figure>
+            </div>
+            <div className="diff-resizer"></div>
+          </figure>
+        </div>
+
+        {/* ------------------ */}
+
+        <div className="col-span-3 xl:col-span-1 card bg-info border-4 m-4">
+          <div className="card-title px-3 pt-3 join">
+            <p className="join-item">*</p>
+            <p className="join-item text-secondary">
+              <strong> | iCAA</strong>
+            </p>
           </div>
 
-{/* ------------------ */}
+          <p className="card-body text-center lg:text-start">
+            Where the i.c.stars Alumni and Residents come together to learn,
+            grow, and share!
+          </p>
+        </div>
 
+        <div className="card card-border col-span-3 xl:col-span-2 bg-warning m-4 p-4">
+          <h1 className="card-title justify-center text-warning-content font-bold">
+            Whats new?
+          </h1>
 
-          <div className="col-span-3 xl:col-span-1 card bg-info border-4 m-4">
-            <div className="card-title px-3 pt-3 join">
-              <p className="join-item">*</p>
-              <p className="join-item text-secondary">
-                <strong> | iCAA</strong>
+          <p className="text-info-content card-body text-center p-3">
+            Stay in-touch with the iCAA events and find opportunities to take
+            advantage of, or help-out with.
+          </p>
+
+          <p className="text-center">
+            <a
+              href="#"
+              target="_blank"
+              className="btn btn-secondary col-span-1 m-3 md:btn-lg btn-outline"
+            >
+              Newsletter
+            </a>
+          </p>
+        </div>
+
+        <div className="card text-info-content col-span-3 md:max-h-[300px] bg-info image-full m-4 shadow-md max-w-screen-md mx-auto">
+          <figure className="sr-only md:not-sr-only">
+            <Image
+              src="/CodeMages.jpg"
+              alt="Team Code Mages"
+              width={500} // Set an appropriate width
+              height={300} // Set an appropriate height
+              layout="responsive" // Dynamically adjusts width and height
+              objectFit="contain" // Ensures the image scales proportionally
+            />
+          </figure>
+          <div className="card-body md:grid md:grid-cols-2 md:items-center m-2 p-2">
+            <div>
+              <h1 className="card-title font-bold">Team Code Mages</h1>
+              <p className="text-info-content card-body p-2">
+                This project was created by a team of talented individuals who
+                are passionate about coding and technology. <br />
+                The team consists of Jonathan Ramirez, who is the lead
+                developer, and other members including <br />
+                <span>
+                  Lead Developer:
+                  <a
+                    href="https://www.linkedin.com/in/bhatianehaa/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-success-content underline"
+                  >
+                    Neha Bhatia
+                  </a>
+                </span>
+                Developer: Flynn Richardson.
               </p>
             </div>
-
-            <p className="card-body text-center lg:text-start">
-              Where the i.c.stars Alumni and Residents come together to learn,
-              grow, and share!
-            </p>
           </div>
+        </div>
 
-          <div className="card card-border col-span-3 xl:col-span-2 bg-warning m-4 p-4">
-            <h1 className="card-title justify-center text-warning-content font-bold">
-              Whats new?
-            </h1>
+        <div className="card col-span-3 xl:col-span-1 card-dash bg-info m-4 p-4 shadow-md">
+          <h1 className="font-bold card-title m-2 p-2">
+            Card with Dashed Border
+          </h1>
+          <p className="card-body m-2 p-2">
+            This card uses the custom card-dash class.
+          </p>
+        </div>
 
-            <p className="text-info-content card-body text-center p-3">
-              Stay in-touch with the iCAA events and find opportunities to take
-              advantage of, or help-out with.
-            </p>
-
-            <p className="text-center">
-              <a
-                href="#"
-                target="_blank"
-                className="btn btn-secondary col-span-1 m-3 md:btn-lg btn-outline"
-              >
-                Newsletter
-              </a>
-            </p>
-          </div>
-
-
-
-
-
-
-          <div className="card text-info-content col-span-3 md:max-h-[300px] bg-info image-full m-4 shadow-md max-w-screen-md mx-auto">
-  <figure className="sr-only md:not-sr-only">
-    <Image
-      src="/CodeMages.jpg"
-      alt="Team Code Mages"
-      width={500} // Set an appropriate width
-      height={300} // Set an appropriate height
-      layout="responsive" // Dynamically adjusts width and height
-      objectFit="contain" // Ensures the image scales proportionally
-    />
-  </figure>
-  <div className="card-body md:grid md:grid-cols-2 m-2 p-2">
-    <div>
-      <h1 className="card-title font-bold">Team Code Mages</h1>
-      <p className="text-info-content card-body p-2">
-        This project was created by a team of talented individuals who are passionate about coding and technology. <br />
-        The team consists of Jonathan Ramirez, who is the lead developer, and other members including <br />
-        <span>
-          Lead Developer:
-          <a
-            href="https://www.linkedin.com/in/bhatianehaa/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-success-content underline"
-          >
-            Neha Bhatia
-          </a>
-        </span>
-        Developer: Flynn Richardson.
-      </p>
-    </div>
-  </div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
+        <div className="card-border col-span-3 xl:col-span-1 shadow-md m-4 p-4">
+          <h1 className="card-title font-bold mb-2">platform</h1>
+          <p className="card-body">
+            This div style here is to use as a raised platform to place smaller
+            cards on to give it a bit of a stacked feel
+          </p>
           <div className="card col-span-3 xl:col-span-1 card-dash bg-info m-4 p-4 shadow-md">
-            <h1 className="font-bold card-title m-2 p-2">
-              Card with Dashed Border
-            </h1>
-            <p className="card-body m-2 p-2">
-              This card uses the custom card-dash class.
-            </p>
-          </div>
-
-
-
-          <div className="card-border col-span-3 xl:col-span-1 shadow-md m-4 p-4">
-            <h1 className="card-title font-bold mb-2">platform</h1>
-            <p className="card-body">
-             This div style here is to use as a raised platform to place smaller cards on to give it a bit of a stacked feel
-            </p>
-            <div className="card col-span-3 xl:col-span-1 card-dash bg-info m-4 p-4 shadow-md">
             <h1 className="font-bold card-title m-2 p-2">
               Card with Dashed Border
             </h1>
@@ -176,33 +154,28 @@ export default function Home() {
             </h1>
             <p className="card-body m-2 p-2">
               This card uses the custom card-dash class.
-            </p>
-          </div>
-          </div>
-
-
-
-
-
-
-          <div className="card bg-base-100 shadow-md col-span-3 xl:col-span-1 m-4 p-4">
-            <h1 className="card-title font-bold mb-2">Welcome 2</h1>
-            <p className="card-body">
-              I, Jonathan Ramirez, a proud Cycle 53 Graduate of i.c.stars,
-              welcome you to my Portfolio-Classroom-Project. I am excited to
-              share my journey with you and showcase my skills and projects.
-            </p>
-          </div>
-
-          <div className="card card-border text-info-content bg-info shadow-md col-span-3 xl:col-span-3 m-4 p-4">
-            <h1 className="font-bold mb-2">Welcome 3</h1>
-            <p className="card-body">
-              I, Jonathan Ramirez, a proud Cycle 53 Graduate of i.c.stars,
-              welcome you to my Portfolio-Classroom-Project. I am excited to
-              share my journey with you and showcase my skills and projects.
             </p>
           </div>
         </div>
+
+        <div className="card bg-base-100 shadow-md col-span-3 xl:col-span-1 m-4 p-4">
+          <h1 className="card-title font-bold mb-2">Welcome 2</h1>
+          <p className="card-body">
+            I, Jonathan Ramirez, a proud Cycle 53 Graduate of i.c.stars, welcome
+            you to my Portfolio-Classroom-Project. I am excited to share my
+            journey with you and showcase my skills and projects.
+          </p>
+        </div>
+
+        <div className="card card-border text-info-content bg-info shadow-md col-span-3 xl:col-span-3 m-4 p-4">
+          <h1 className="font-bold mb-2">Welcome 3</h1>
+          <p className="card-body">
+            I, Jonathan Ramirez, a proud Cycle 53 Graduate of i.c.stars, welcome
+            you to my Portfolio-Classroom-Project. I am excited to share my
+            journey with you and showcase my skills and projects.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
