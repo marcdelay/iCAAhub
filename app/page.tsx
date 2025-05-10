5f52c9bb111e9bfa0d5af7ca9fc6301d7803ba47
Jonathan <jramirez@icstars.org>
2025-05-10 06:38:16 -0500

    trimmed out useless divs

--- app/page.tsx
+++ app/page.tsx
@@ -18,58 +18,40 @@ export default function Home() {
           <div className="col-span-1 md:col-span-4">
             <AnnouncementDash />
           </div>
-          </div>
-
-          <div className="card-border col-span-3 text-info-content xl:col-span-1 shadow-md m-4 p-4">
-            <h1 className="card-title font-bold mb-2">platform</h1>
-            <p className="card-body">
-              This div style here is to use as a raised platform to place
-              smaller cards on to give it a bit of a stacked feel
-            </p>
-            <div className="card col-span-3 xl:col-span-1 card-dash bg-info m-4 p-4 shadow-md">
-              <h1 className="font-bold card-title m-2 p-2">
-                Card with Dashed Border
-              </h1>
-              <p className="card-body m-2 p-2">
-                This card uses the custom card-dash class.
-              </p>
-            </div>
-            <div className="card col-span-3 xl:col-span-1 card-dash bg-info m-4 p-4 shadow-md">
-              <h1 className="font-bold card-title m-2 p-2">
-                Card with Dashed Border
-              </h1>
-              <p className="card-body m-2 p-2">
-                This card uses the custom card-dash class.
-              </p>
-            </div>
-            <div className="card col-span-3 xl:col-span-1 card-dash bg-info m-4 p-4 shadow-md">
-              <h1 className="font-bold card-title m-2 p-2">
-                Card with Dashed Border
-              </h1>
-              <p className="card-body m-2 p-2">
-                This card uses the custom card-dash class.
-              </p>
-            </div>
-          </div>
-
-          <div className="card bg-base-100 shadow-md col-span-3 xl:col-span-2 m-4 p-4">
-            <h1 className="card-title font-bold mb-2">Welcome 2</h1>
-            <p className="card-body">
-              I, Jonathan Ramirez, a proud Cycle 53 Graduate of i.c.stars,
-              welcome you to my Portfolio-Classroom-Project. I am excited to
-              share my journey with you and showcase my skills and projects.
-            </p>
-          </div>
-
-          <div className="card card-border text-info-content bg-info shadow-md col-span-3 xl:col-span-3 m-4 p-4">
-            <h1 className="font-bold mb-2">Welcome 3</h1>
-            <p className="card-body">
-              I, Jonathan Ramirez, a proud Cycle 53 Graduate of i.c.stars,
-              welcome you to my Portfolio-Classroom-Project. I am excited to
-              share my journey with you and showcase my skills and projects.
-            </p>
-          </div>
         </div>
+
+        <div className="card-border col-span-3 text-info-content xl:col-span-1 shadow-md m-4 mb-0 p-4">
+          <h1 className="card-title font-bold mb-2">platform</h1>
+          <p className="card-body">
+            This div style here is to use as a raised platform to place smaller
+            cards on to give it a bit of a stacked feel
+          </p>
+          <div className="card col-span-3 xl:col-span-1 card-dash bg-info m-4 p-4 shadow-md">
+            <h1 className="font-bold card-title m-2 p-2">
+              Card with Dashed Border
+            </h1>
+            <p className="card-body m-2 p-2">
+              This card uses the custom card-dash class.
+            </p>
+          </div>
+          <div className="card col-span-3 xl:col-span-1 card-dash bg-info m-4 p-4 shadow-md">
+            <h1 className="font-bold card-title m-2 p-2">
+              Card with Dashed Border
+            </h1>
+            <p className="card-body m-2 p-2">
+              This card uses the custom card-dash class.
+            </p>
+          </div>
+          <div className="card col-span-3 xl:col-span-1 card-dash bg-info m-4 p-4 shadow-md">
+            <h1 className="font-bold card-title m-2 p-2">
+              Card with Dashed Border
+            </h1>
+            <p className="card-body m-2 p-2">
+              This card uses the custom card-dash class.
+            </p>
+          </div>
+        </div>     
+      </div>
     </main>
   );
 }
--- components/AnnouncementDash.tsx
+++ components/AnnouncementDash.tsx
@@ -34,19 +34,21 @@ const AnnouncementDash = () => {
   return (
     <div>
       <div className="card card-border items-center bg-info m-4">
-        <div className="card-title text-info-content border-b-2 border-info-content w-full mt-2 mb-4 items-center justify-center">
+        <div className="card-title text-info-content border-b-2 border-info-content w-full mt-2 items-center justify-center">
           <h1 className="font-bold">ANNOUNCEMENTS</h1>
         </div>
 
         <div className="grid md:grid-cols-3 h-full card-body text-info-content">
-          <div className="md:border-r-1 border-b-1 md:border-b-0 text-start p-3 col-span-1">
+          <div className="md:border-r-1 border-b-1 md:border-b-0 text-start col-span-1">
+            
+            <h2 className="font-bold">Notes From the Executive Board:</h2>
+            <br />
             <p>Dear Alumni,</p>
             <br />
             <p>
               As we step into the month of May, I invite each of us to reflect
               on the light we carry...
             </p>
-            <br />
             <p>
               <Link className="text-blue-400 underline" href="/newsletter">
                 Read more
@@ -55,10 +57,12 @@ const AnnouncementDash = () => {
           </div>
           <div className="text-center col-span-1">
             <h2 className="font-bold">Latest Announcement</h2>
+            <br />
             {latestPost ? (
               <div>
                 <h3 className="text-lg font-semibold">{latestPost.title}</h3>
                 <p className="text-sm">{latestPost.content}</p>
+                <br />
                 <Link
                   className="text-blue-400 underline"
                   href="/announcements"
